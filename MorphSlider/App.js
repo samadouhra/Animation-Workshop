4ecd44fd704c54ed39867dfafb20e6f1698cbd96
abdessamad ouhraali <62210295+samouhra@users.noreply.github.com>
2022-05-09 09:52:57 +0100

    first-commit

---
+++ MorphSlider/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Navigator from './navigator';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Navigator />
+    </View>
+  );
+}
